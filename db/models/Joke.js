d28a32d32edceff60a49313fac4ef6e190116650
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-05-10 14:55:37 +0200

    add demo code

---
+++ db/models/Joke.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const jokeSchema = new Schema({
+  joke: {
+    type: String,
+    required: true,
+  },
+});
+
+const Joke = mongoose.models.Joke || mongoose.model("Joke", jokeSchema);
+
+export default Joke;
+
+//db.jokes.find()
+
+// in mongoose:
+// Joke.find()
--- pages/api/jokes/index.js
+++ pages/api/jokes/index.js
@@ -1,5 +1,15 @@
-import { jokes } from "../../../lib/data.js";
+import Joke from "../../../db/models/Joke.js";
+import dbConnect from "../../../db/connect.js";
 
-export default function handler(request, response) {
-  response.status(200).json(jokes);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const jokes = await Joke.find();
+
+    response.status(200).json(jokes);
+    return;
+  }
+
+  response.status(405).json({ status: "Wrong request type." });
 }
